eac9cf4ff6eb02b0cc5176adb2825eea8a652cb2
carmine.rumma <carmine.rumma@healthtouch.eu>
2017-10-11 16:36:59 +0200

    Path Environment Fix

--- conf/gallery/config/path.php
+++ conf/gallery/config/path.php
@@ -1,10 +1,10 @@
 <?php
-if ($_SERVER["HTTP_HOST"] === "vgallery.alex")
+if ($_SERVER["HTTP_HOST"] === "dev.ffcmsmaster.local")
 {
-    define("FF_DISK_PATH", 'D:/htdocs/vgallery');
+    define("FF_DISK_PATH", __TOP_DIR__);
     define("FF_SITE_PATH", '');
 
-    define("DISK_UPDIR", 'D:/htdocs/vgallery/uploads');
+    define("DISK_UPDIR", __TOP_DIR__ . DS . 'uploads');
     define("SITE_UPDIR", '/uploads');
 }
 else
