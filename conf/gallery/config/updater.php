71724c29b275ec174a51c5f55617d190268f2462
Alessandro Stucchi <wolfgan@gmail.com>
2017-10-18 17:44:19 +0200

    Remove Old config

--- conf/gallery/config/updater.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define("MASTER_SITE", 'www.blueocarina.net');
-
-    define("FTP_USERNAME", 'bonet');
-    define("FTP_PASSWORD", 'Gomma258');
-    define("FTP_PATH", '/httpdocs'); 
-    
-    $config_check["updater"] = true;
