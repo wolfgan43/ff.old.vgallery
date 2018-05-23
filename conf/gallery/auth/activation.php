3d9d81e29379c91784995210b47fd5ef68b3685b
wolfgan <wolfgan@gmail.com>
2018-05-23 17:40:42 +0200

    fix Activation

--- conf/gallery/auth/activation.php
+++ conf/gallery/auth/activation.php
@@ -29,7 +29,7 @@ $framework_css = mod_sec_get_framework_css();
 $tiny_lang_code = strtolower(substr(FF_LOCALE, 0, 2));
 
 $mod_sec_login = $cm->router->getRuleById("mod_sec_login");
-$mod_sec_activation = ($cm->router->getRuleById("mod_sec_activation_" . $tiny_lang_code) 
+$mod_sec_activation = ($cm->router->getRuleById("mod_sec_activation_" . $tiny_lang_code)
                         ? $cm->router->getRuleById("mod_sec_activation_" . $tiny_lang_code)
                         : $cm->router->getRuleById("mod_sec_activation")
                     );
@@ -52,7 +52,7 @@ if(MOD_SEC_CSS_PATH !== false && isset($cm->router->matched_rules["mod_sec_login
             , "path" => $ret["path"]
     ));
 }
-    
+
 $tpl = ffTemplate::factory(get_template_cascading($user_path, "activation.html", "/login"));
 $tpl->load_file("activation.html", "main");
 $tpl->set_var("site_path", FF_SITE_PATH);
@@ -63,9 +63,9 @@ if($framework_css["component"]["grid"]) {
     if(is_array($framework_css["component"]["grid"]))
         $component_class["grid"] = cm_getClassByFrameworkCss($framework_css["component"]["grid"], "col");
     else {
-        $component_class["grid"] = cm_getClassByFrameworkCss("", $framework_css["component"]["grid"]);      
+        $component_class["grid"] = cm_getClassByFrameworkCss("", $framework_css["component"]["grid"]);
     }
-}   
+}
 
 $sError = "";
 
@@ -82,27 +82,27 @@ if (!strlen($ret_url) || strpos($ret_url, $mod_sec_activation->reverse) !== fals
 
 $tpl->set_var("ret_url", $ret_url);
 
-$tpl->set_var("container_class", implode(" ", array_filter($component_class))); 
+$tpl->set_var("container_class", implode(" ", array_filter($component_class)));
 
-if(!MOD_SEC_LOGO) {  
-    $framework_css["login"]["def"]["col"] = array( 
+if(!MOD_SEC_LOGO) {
+    $framework_css["login"]["def"]["col"] = array(
                                             "xs" => 12
                                             , "sm" => 12
                                             , "md" => 12
-                                            , "lg" => 12 
+                                            , "lg" => 12
                                         );
 	$framework_css["logout"] = false;
-    $framework_css["inner-wrap"]["col"] = array( 
+    $framework_css["inner-wrap"]["col"] = array(
                                             "xs" => 6
                                             , "sm" => 6
                                             , "md" => 6
-                                            , "lg" => 6 
+                                            , "lg" => 6
                                         );
-    $framework_css["inner-wrap"]["push"] = array( 
+    $framework_css["inner-wrap"]["push"] = array(
                                             "xs" => 3
                                             , "sm" => 3
                                             , "md" => 3
-                                            , "lg" => 3 
+                                            , "lg" => 3
                                         );
 }
 $tpl->set_var("inner_wrap_class", cm_getClassByDef($framework_css["inner-wrap"]));
@@ -128,7 +128,7 @@ else
                 , 'formName'    : 'ffActivation'
 				, 'injectid'    : 'ffActivation'
                 , 'url'            : '" . $_SERVER["REQUEST_URI"] . "'
-			}); 
+			});
 		});");
 }*/
 $tpl->set_var("login_class", cm_getClassByDef($framework_css["login"]["def"]));
@@ -141,7 +141,7 @@ switch($frmAction) {
             $sError = ffTemplate::_get_word_by_code("error_activation_fieldempty");
         }
         else
-        {   
+        {
             if (MOD_SEC_MULTIDOMAIN && MOD_SEC_LOGIN_DOMAIN)
             {
                 if (strlen($domain))
@@ -170,7 +170,7 @@ switch($frmAction) {
                     $db = mod_security_get_db_by_domain($ID_domain);
                 else
                     $db = mod_security_get_main_db();
-                                        
+
                 $sSQL = "SELECT
                             " . $options["table_name"] . ".ID
                             , " . $options["table_name"] . ".username
@@ -194,27 +194,27 @@ switch($frmAction) {
 
                 if (MOD_SEC_MULTIDOMAIN && !MOD_SEC_MULTIDOMAIN_EXTERNAL_DB && MOD_SEC_LOGIN_DOMAIN)
                     $sSQL .= " AND " . $options["table_name"] . ".ID_domains = " . $db->toSql($ID_domain);
-                
+
                 if (MOD_SEC_EXCLUDE_SQL)
                     $sSQL .= " AND " . $options["table_name"] . ".ID " . MOD_SEC_EXCLUDE_SQL;
-                
+
                 $sSQL .= " ORDER BY ID DESC";
 
-                $db->query($sSQL);   
+                $db->query($sSQL);
                 if ($db->nextRecord())
                 {
                     if($db->getField("status")->getValue() > 0)
                     {
                         $sError = ffTemplate::_get_word_by_code("error_activation_already_active");
                     }
-                    else 
+                    else
                     {
                         $ID = $db->getField("ID", "Number", true);
 
                         $activation = $db->getField("activation")->getValue();
                         $username = $db->getField("username")->getValue();
                         $email = $db->getField("email")->getValue();
-                        
+
                         $ID_email_activation = $db->getField("ID_email_activation", "Number", true);
 						if(!$ID_email_activation) {
                             $struct_email = email_system("account activation");
@@ -239,29 +239,29 @@ switch($frmAction) {
 
                         if(is_array($to_active) && count($to_active) > 0) {
                             $rnd_active = mod_sec_createRandomPassword();
-                    
+
                             $sSQL = "UPDATE " . $options["table_name"] . " SET active_sid = PASSWORD(" . $db->toSql($rnd_active, "Text") . ") WHERE ID = " . $db->toSql($ID, "Number");
                             $db->execute($sSQL);
 
-                            /**                                               
+                            /**
                             * TODO:Da togliere gestione utente e fonderla con anagraph
-                            */ 
+                            */
                             if($options["table_name"] != "anagraph") {
 	                            $sSQL = "UPDATE anagraph SET
 	                                            active_sid = PASSWORD(" . $db->toSql($rnd_active, "Text") . ")
 	                                        WHERE uid = " . $db->toSql($ID, "Number");
-	                            $db->execute($sSQL);                                              
+	                            $db->execute($sSQL);
 							}
 
                             $fields_activation["activation"]["username"] = $username;
                             $fields_activation["activation"]["email"] = $email;
-                            $fields_activation["activation"]["link"] = "http://" . DOMAIN_INSET . FF_SITE_PATH . $mod_sec_activation->reverse  . "?frmAction=activation&sid=" . urlencode($rnd_active);
+                            $fields_activation["activation"]["link"] = "http" . ($_SERVER["HTTPS"] ? "s": "") . "://" . DOMAIN_INSET . FF_SITE_PATH . $mod_sec_activation->reverse  . "?frmAction=activation&sid=" . urlencode($rnd_active);
 
                             if(check_function("process_mail")) {
                                 //$rc_from_activation = process_mail(email_system("account activation"), $to_active, NULL, NULL, $fields_activation, null, null, null, false, null, true);
-                        	    $rc_activation = process_mail($ID_email_activation, $to_active, NULL, NULL, $fields_activation);
+                                $rc_activation = process_mail($ID_email_activation, $to_active, NULL, NULL, $fields_activation, null, null, null, false, null, true);
                             }
-                            
+
                             if(!$rc_activation) {
                                 ffRedirect(FF_SITE_PATH . $mod_sec_activation->reverse . "?frmAction=active&desc=sendmail&ret_url=" . urlencode(FF_SITE_PATH . "/"));
                             } else {
@@ -292,14 +292,14 @@ switch($frmAction) {
                         LEFT JOIN module_register ON module_register.ID = " . $options["table_name"] . ".ID_module_register
         			WHERE active_sid = PASSWORD(" . $db->toSql(new ffData($sid)) . ")");
         if ($db->nextRecord())
-        {   
+        {
             $ID = $db->getField("ID", "Number", true);
-            
+
             if($db->getField("status")->getValue() > 0)
             {
                 $sError = ffTemplate::_get_word_by_code("error_activation_already_active");
             }
-            else 
+            else
             {
                 $to[0]["name"] = $db->getField("username", "Text", true);
                 $to[0]["mail"] = $db->getField("email", "Text", true);
@@ -310,26 +310,26 @@ switch($frmAction) {
                 $fields["account"]["email"] = $db->getField("email", "Text", true);
                 $fields["account"]["name"] = $db->getField("name", "Text", true);
                 $fields["account"]["surname"] = $db->getField("surname", "Text", true);
-                $fields["activationlogin"]["link"] = "http://" . DOMAIN_INSET . FF_SITE_PATH . $mod_sec_login->reverse . "?username=" . urlencode($fields["account"]["username"]);
+                $fields["activationlogin"]["link"] = "http" . ($_SERVER["HTTPS"] ? "s": "") . "://" . DOMAIN_INSET . FF_SITE_PATH . $mod_sec_login->reverse . "?username=" . urlencode($fields["account"]["username"]);
 
                 if(check_function("process_mail")) {
 					$ID_email = $db->getField("ID_email", "Number", true);
 					if(!$ID_email) {
                         $struct_email = email_system("account registration");
                         $ID_email = $struct_email["ID"];
-					}                
-                
-                    $rc_from_account = process_mail($ID_email, $to, NULL, NULL, $fields, null, null, null, false, null, true);
-                    $rc_account = process_mail($ID_email, $to, NULL, NULL, $fields);
+                    }
+
+                    //$rc_from_account = process_mail($ID_email, $to, NULL, NULL, $fields, null, null, null, false, null, true);
+                    $rc_account = process_mail($ID_email, $to, NULL, NULL, $fields, null, null, null, false, null, true);
                 }
                 $sSQL = "UPDATE " . $options["table_name"] . " SET
                                 status = 1 
                             WHERE ID = " . $db->toSql($ID, "Number");
                 $db->execute($sSQL);
-                    
-                /**                                               
+
+                /**
                 * TODO:Da togliere gestione utente e fonderla con anagraph
-                */ 
+                */
                 if($options["table_name"] != "anagraph") {
 	                $sSQL = "UPDATE anagraph SET
 	                                status = 1 
@@ -345,17 +345,17 @@ switch($frmAction) {
                 ffRedirect(FF_SITE_PATH . $mod_sec_activation->reverse . "?frmAction=active&desc=success&ret_url=" . urlencode(FF_SITE_PATH . "/"));
             }
         }
-        else 
-            {
-              $sError = ffTemplate::_get_word_by_code("error_activation_sid_wrong");
-            }
+        else
+        {
+            $sError = ffTemplate::_get_word_by_code("error_activation_sid_wrong");
+        }
         $display_activation = true;
         break;
     case "active":
-    	$display_activation = false;
-    	break;
+        $display_activation = false;
+        break;
     default:
-		$display_activation = true;
+        $display_activation = true;
 }
 
 if ($layout_settings["AREA_LOGIN_ACTIVATION_SHOW_TITLE"])
@@ -381,7 +381,7 @@ if (strlen($sError)) {
 	if ($cm->isXHR())
     {
 		$cm->jsonAddResponse(array(
-				"success" => false 
+				"success" => false
 				, "modules" => array(
 					"security" => array(
 						"action" => "activation request"
@@ -406,9 +406,9 @@ if(MOD_SEC_LOGO) {
     } else {
         if(is_file(FF_DISK_PATH . MOD_SEC_LOGO_PATH))
             $logo_url = MOD_SEC_LOGO_PATH;
-        elseif(is_file(FF_THEME_DISK_PATH . "/" . $cm->oPage->getTheme() . "/images/logo/login.svg")) 
+        elseif(is_file(FF_THEME_DISK_PATH . "/" . $cm->oPage->getTheme() . "/images/logo/login.svg"))
             $logo_url = FF_SITE_PATH . FF_THEME_DIR . "/" . $cm->oPage->getTheme() . "/images/logo/login.svg";
-        elseif(is_file(FF_THEME_DISK_PATH . "/" . $cm->oPage->getTheme() . "/images/logo/login.png")) 
+        elseif(is_file(FF_THEME_DISK_PATH . "/" . $cm->oPage->getTheme() . "/images/logo/login.png"))
             $logo_url = FF_SITE_PATH . FF_THEME_DIR . "/" . $cm->oPage->getTheme() . "/images/logo/login.png";
         elseif(is_file(FF_THEME_DISK_PATH . "/" . cm_getMainTheme() . "/images/logo/login.gif"))
             $logo_url = FF_SITE_PATH . FF_THEME_DIR . "/" . cm_getMainTheme() . "/images/logo/login.gif";
